de01a2414d4a55f84b9a01eae86c0ce5e8326564
jack <jack9robinson@gmail.com>
2022-11-24 13:50:39 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  const [cocktails, setCocktails] = useState([])
+  const [userInput, setUserInput] = useState("")
+  const [isLoading, setIsLoading] = useState(true)
+  const [isFound, setIsFound] = useState(false)
+
+  async function handleApiCall(e) {
+    e.preventDefault()
+    console.log(process.env.REACT_APP_API_URL + `/search.php?s=margarita`)
+    await fetch(process.env.REACT_APP_API_URL + `/search.php?s=${userInput}`, {'method': "GET"})
+    .then(res => res.json())
+    .then(data => setCocktails(data.drinks))
+    
+    
+    
+    
+        
+      // if empty flag it
+      // if not show results
+    
+    // if first search is empty search, search input as ingredient
+
+    // if second search is empty, display "sorry, nothing was found"
+
+  }
+  const onChange = (event) => {
+    setUserInput(event.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleApiCall}>
+        <label htmlFor='userInput'></label>
+        <input name='userinput' type='text' value={userInput} onChange={onChange}></input>
+        <input type="submit" value="Search"></input>
+      </form> 
+      
+      <ul>
+        {cocktails.map((cocktail, index) => {
+          <li key={index}>{cocktail.strDrink} </li>
+        })}
+      </ul>
+      <h1>Test</h1>
     </div>
   );
 }
